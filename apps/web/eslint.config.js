6d9a23a7728dfc0c2eb9c4aa483e6b82c1d9a48c
lordpluha <Tesluakevlad@gmail.com>
2025-06-06 21:48:18 +0200

    Add architecture validation

--- apps/web/eslint.config.js
+++ apps/web/eslint.config.js
@@ -1,4 +1,8 @@
-import { nextJsConfig } from "@spotify/eslint/next-js";
+import { defineConfig } from 'eslint/config'
+import { nextJsConfig } from '@spotify/eslint/next-js'
+import { FSDConfig } from '@spotify/eslint/architecture'
 
 /** @type {import("eslint").Linter.Config} */
-export default nextJsConfig;
+export default defineConfig({
+  extends: [nextJsConfig, FSDConfig]
+})
