ebc90f57f558fcaf445f4348b38e630d884943e9
lordpluha <Tesluakevlad@gmail.com>
2025-07-08 08:53:50 +0200

    Generate new types

--- packages/contracts/src/api/v1.ts
+++ packages/contracts/src/api/v1.ts
@@ -29,21 +29,89 @@ export interface paths {
         };
         get?: never;
         put?: never;
-        post: operations["AuthController_signIn"];
+        /** User login */
+        post: operations["AuthController_login"];
         delete?: never;
         options?: never;
         head?: never;
         patch?: never;
         trace?: never;
     };
-    "/auth/profile": {
+    "/auth/registration": {
         parameters: {
             query?: never;
             header?: never;
             path?: never;
             cookie?: never;
         };
-        get: operations["AuthController_getProfile"];
+        get?: never;
+        put?: never;
+        /** User registration */
+        post: operations["AuthController_registration"];
+        delete?: never;
+        options?: never;
+        head?: never;
+        patch?: never;
+        trace?: never;
+    };
+    "/auth/logout": {
+        parameters: {
+            query?: never;
+            header?: never;
+            path?: never;
+            cookie?: never;
+        };
+        get?: never;
+        put?: never;
+        /** User logout */
+        post: operations["AuthController_logout"];
+        delete?: never;
+        options?: never;
+        head?: never;
+        patch?: never;
+        trace?: never;
+    };
+    "/auth/refresh": {
+        parameters: {
+            query?: never;
+            header?: never;
+            path?: never;
+            cookie?: never;
+        };
+        get?: never;
+        put?: never;
+        /** Refresh access token */
+        post: operations["AuthController_refresh"];
+        delete?: never;
+        options?: never;
+        head?: never;
+        patch?: never;
+        trace?: never;
+    };
+    "/users/{id}": {
+        parameters: {
+            query?: never;
+            header?: never;
+            path?: never;
+            cookie?: never;
+        };
+        get: operations["UsersController_getById"];
+        put?: never;
+        post?: never;
+        delete?: never;
+        options?: never;
+        head?: never;
+        patch?: never;
+        trace?: never;
+    };
+    "/users/{username}": {
+        parameters: {
+            query?: never;
+            header?: never;
+            path?: never;
+            cookie?: never;
+        };
+        get: operations["UsersController_getByUsername"];
         put?: never;
         post?: never;
         delete?: never;
@@ -55,7 +123,32 @@ export interface paths {
 }
 export type webhooks = Record<string, never>;
 export interface components {
-    schemas: never;
+    schemas: {
+        LoginDto: {
+            /**
+             * @description User email
+             * @example user@example.com
+             */
+            email: string;
+            /**
+             * @description User password
+             * @example password123
+             */
+            password: string;
+        };
+        RegistrationDto: {
+            /**
+             * @description New user email
+             * @example newuser@example.com
+             */
+            email: string;
+            /**
+             * @description New user password
+             * @example password123
+             */
+            password: string;
+        };
+    };
     responses: never;
     parameters: never;
     requestBodies: never;
@@ -73,7 +166,78 @@ export interface operations {
         };
         requestBody?: never;
         responses: {
+            default: {
+                headers: {
+                    [name: string]: unknown;
+                };
+                content: {
+                    "application/json": string;
+                };
+            };
+        };
+    };
+    AuthController_login: {
+        parameters: {
+            query?: never;
+            header?: never;
+            path?: never;
+            cookie?: never;
+        };
+        requestBody: {
+            content: {
+                "application/json": components["schemas"]["LoginDto"][];
+            };
+        };
+        responses: {
+            /** @description Successfully logged in */
             200: {
+                headers: {
+                    [name: string]: unknown;
+                };
+                content: {
+                    /** @example {
+                     *       "status": "ok"
+                     *     } */
+                    "application/json": unknown;
+                };
+            };
+            /** @description Validation error */
+            400: {
+                headers: {
+                    [name: string]: unknown;
+                };
+                content: {
+                    /** @example {
+                     *       "statusCode": 400,
+                     *       "message": [
+                     *         "email must be an email"
+                     *       ],
+                     *       "error": "Bad Request"
+                     *     } */
+                    "application/json": unknown;
+                };
+            };
+            /** @description Invalid credentials */
+            401: {
+                headers: {
+                    [name: string]: unknown;
+                };
+                content: {
+                    /** @example {
+                     *       "statusCode": 401,
+                     *       "message": "Invalid credentials"
+                     *     } */
+                    "application/json": unknown;
+                };
+            };
+            /** @description Server error */
+            500: {
+                headers: {
+                    [name: string]: unknown;
+                };
+                content?: never;
+            };
+            default: {
                 headers: {
                     [name: string]: unknown;
                 };
@@ -81,7 +245,76 @@ export interface operations {
             };
         };
     };
-    AuthController_signIn: {
+    AuthController_registration: {
+        parameters: {
+            query?: never;
+            header?: never;
+            path?: never;
+            cookie?: never;
+        };
+        requestBody: {
+            content: {
+                "application/json": components["schemas"]["RegistrationDto"][];
+            };
+        };
+        responses: {
+            /** @description Successfully registered */
+            200: {
+                headers: {
+                    [name: string]: unknown;
+                };
+                content: {
+                    /** @example {
+                     *       "status": 200
+                     *     } */
+                    "application/json": unknown;
+                };
+            };
+            /** @description Validation error */
+            400: {
+                headers: {
+                    [name: string]: unknown;
+                };
+                content: {
+                    /** @example {
+                     *       "statusCode": 400,
+                     *       "message": [
+                     *         "email must be an email"
+                     *       ],
+                     *       "error": "Bad Request"
+                     *     } */
+                    "application/json": unknown;
+                };
+            };
+            /** @description User already exists */
+            409: {
+                headers: {
+                    [name: string]: unknown;
+                };
+                content: {
+                    /** @example {
+                     *       "statusCode": 409,
+                     *       "message": "User already exists"
+                     *     } */
+                    "application/json": unknown;
+                };
+            };
+            /** @description Server error */
+            500: {
+                headers: {
+                    [name: string]: unknown;
+                };
+                content?: never;
+            };
+            default: {
+                headers: {
+                    [name: string]: unknown;
+                };
+                content?: never;
+            };
+        };
+    };
+    AuthController_logout: {
         parameters: {
             query?: never;
             header?: never;
@@ -90,7 +323,33 @@ export interface operations {
         };
         requestBody?: never;
         responses: {
+            /** @description Successfully logged out */
             200: {
+                headers: {
+                    [name: string]: unknown;
+                };
+                content: {
+                    /** @example {
+                     *       "status": "logged out"
+                     *     } */
+                    "application/json": unknown;
+                };
+            };
+            /** @description Unauthorized */
+            401: {
+                headers: {
+                    [name: string]: unknown;
+                };
+                content?: never;
+            };
+            /** @description Server error */
+            500: {
+                headers: {
+                    [name: string]: unknown;
+                };
+                content?: never;
+            };
+            default: {
                 headers: {
                     [name: string]: unknown;
                 };
@@ -98,7 +357,7 @@ export interface operations {
             };
         };
     };
-    AuthController_getProfile: {
+    AuthController_refresh: {
         parameters: {
             query?: never;
             header?: never;
@@ -107,7 +366,90 @@ export interface operations {
         };
         requestBody?: never;
         responses: {
+            /** @description Token refreshed */
             200: {
+                headers: {
+                    [name: string]: unknown;
+                };
+                content: {
+                    /** @example {
+                     *       "status": "token refreshed"
+                     *     } */
+                    "application/json": unknown;
+                };
+            };
+            /** @description Refresh token not provided */
+            400: {
+                headers: {
+                    [name: string]: unknown;
+                };
+                content: {
+                    /** @example {
+                     *       "statusCode": 400,
+                     *       "message": "refresh token is not provided, please login again"
+                     *     } */
+                    "application/json": unknown;
+                };
+            };
+            /** @description Invalid or expired refresh token */
+            401: {
+                headers: {
+                    [name: string]: unknown;
+                };
+                content: {
+                    /** @example {
+                     *       "statusCode": 401,
+                     *       "message": "Invalid or expired refresh token"
+                     *     } */
+                    "application/json": unknown;
+                };
+            };
+            /** @description Server error */
+            500: {
+                headers: {
+                    [name: string]: unknown;
+                };
+                content?: never;
+            };
+            default: {
+                headers: {
+                    [name: string]: unknown;
+                };
+                content?: never;
+            };
+        };
+    };
+    UsersController_getById: {
+        parameters: {
+            query?: never;
+            header?: never;
+            path: {
+                id: string;
+            };
+            cookie?: never;
+        };
+        requestBody?: never;
+        responses: {
+            default: {
+                headers: {
+                    [name: string]: unknown;
+                };
+                content?: never;
+            };
+        };
+    };
+    UsersController_getByUsername: {
+        parameters: {
+            query?: never;
+            header?: never;
+            path: {
+                username: string;
+            };
+            cookie?: never;
+        };
+        requestBody?: never;
+        responses: {
+            default: {
                 headers: {
                     [name: string]: unknown;
                 };
