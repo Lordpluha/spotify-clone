b0b81d7f62d23607a73560902f2534bddd6af2fa
lordpluha <Tesluakevlad@gmail.com>
2025-07-17 07:06:01 +0200

    Remove start instructions for storybook

--- packages/ui/.storybook/preview.ts
+++ packages/ui/.storybook/preview.ts
@@ -1,15 +1,15 @@
-import 'tailwindcss/tailwind.css';
+import 'tailwindcss/tailwind.css'
 import type { Preview } from '@storybook/react-vite'
 
 const preview: Preview = {
   parameters: {
     controls: {
       matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/i,
-      },
-    },
-  },
-};
+        color: /(background|color)$/i,
+        date: /Date$/i
+      }
+    }
+  }
+}
 
-export default preview;
+export default preview
