6960b8c4b82483568fd377b9a84c53ee18161df4
lordpluha <Tesluakevlad@gmail.com>
2025-07-18 11:54:03 +0200

    feat(custom): add basic entities to admin panel
    ISSUES CLOSED: #69

--- apps/admin/src/admin/options.ts
+++ apps/admin/src/admin/options.ts
@@ -30,6 +30,106 @@ const options: AdminJSOptions = {
         },
       },
     },
+    {
+      resource: { model: getModelByName('Album'), client: prisma },
+      options: {
+        navigation: {
+          name: 'Albums',
+          icon: 'Album',
+        },
+        properties: {
+          id: {
+            isVisible: {
+              list: true,
+              filter: true,
+              show: true,
+              edit: false,
+              new: false,
+            },
+          },
+        },
+      },
+    },
+    {
+      resource: { model: getModelByName('Artist'), client: prisma },
+      options: {
+        navigation: {
+          name: 'Artists',
+          icon: 'Artist',
+        },
+        properties: {
+          id: {
+            isVisible: {
+              list: true,
+              filter: true,
+              show: true,
+              edit: false,
+              new: false,
+            },
+          },
+        },
+      },
+    },
+    {
+      resource: { model: getModelByName('Playlist'), client: prisma },
+      options: {
+        navigation: {
+          name: 'Playlists',
+          icon: 'Playlist',
+        },
+        properties: {
+          id: {
+            isVisible: {
+              list: true,
+              filter: true,
+              show: true,
+              edit: false,
+              new: false,
+            },
+          },
+        },
+      },
+    },
+    {
+      resource: { model: getModelByName('Session'), client: prisma },
+      options: {
+        navigation: {
+          name: 'Sessions',
+          icon: 'Session',
+        },
+        properties: {
+          id: {
+            isVisible: {
+              list: true,
+              filter: true,
+              show: true,
+              edit: false,
+              new: false,
+            },
+          },
+        },
+      },
+    },
+    {
+      resource: { model: getModelByName('Track'), client: prisma },
+      options: {
+        navigation: {
+          name: 'Tracks',
+          icon: 'Track',
+        },
+        properties: {
+          id: {
+            isVisible: {
+              list: true,
+              filter: true,
+              show: true,
+              edit: false,
+              new: false,
+            },
+          },
+        },
+      },
+    },
   ],
   databases: [],
 };
