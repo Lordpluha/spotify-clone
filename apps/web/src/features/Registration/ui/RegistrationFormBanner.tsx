bc5520e3e448b7191edc6bcb0e24c823c0022807
andreo-kalashnello <kotikfop@gmail.com>
2025-08-17 14:49:39 +0300

    fix slider and add eye icon to form

--- apps/web/src/features/Registration/ui/RegistrationFormBanner.tsx
+++ apps/web/src/features/Registration/ui/RegistrationFormBanner.tsx
@@ -1,12 +1,8 @@
-<<<<<<< HEAD
 'use client'
 
 import { Carousel, CarouselContent, CarouselItem } from '@spotify/ui'
 import { useRef } from 'react'
 import Autoplay from 'embla-carousel-autoplay'
-=======
-import { Carousel, CarouselContent, CarouselItem, CarouselPrevious, CarouselNext } from '@spotify/ui'
->>>>>>> e4d4eeebe1b68cbd647241326e2f060b4f77a073
 
 const slides = [
   { src: '/images/banner-1.jpg', alt: 'Banner 1' },
@@ -16,45 +12,6 @@ const slides = [
 ]
 
 export const RegistrationFormBanner = () => {
-<<<<<<< HEAD
-  const plugin = useRef(
-    Autoplay({
-      delay: 3000,
-      stopOnInteraction: false,
-      stopOnMouseEnter: false,
-    })
-  )
-
-  return (
-    <div className="basis-[50%] overflow-hidden rounded-[0_10px_10px_0] max-lg:hidden absolute h-full top-0 right-5 left-1/2 flex-grow-0">
-      <Carousel
-        orientation="vertical"
-        opts={{
-          loop: true,
-          align: "start",
-          skipSnaps: false,
-          dragFree: false,
-          containScroll: "trimSnaps",
-          watchDrag: false,
-        }}
-        plugins={[plugin.current]}
-        className="h-full"
-      >
-        <CarouselContent className="h-full w-full -mt-0">
-          {slides.map(({ src, alt }, i) => (
-            <CarouselItem
-              key={i}
-              className="relative h-full basis-full"
-            >
-              <div className="h-full w-full">
-                <img
-                  src={src}
-                  alt={alt}
-                  className="w-full h-full object-cover select-none"
-                  draggable={false}
-                />
-              </div>
-=======
   return (
     <div className="basis-[50%] overflow-hidden rounded-[0_10px_10px_0] max-lg:hidden absolute h-full top-0 right-0 flex-grow-0">
       <Carousel orientation="vertical" opts={{ loop: true, align: "start" }} >
@@ -70,7 +27,6 @@ export const RegistrationFormBanner = () => {
                 className="w-full h-full object-cover select-none"
                 draggable={false}
               />
->>>>>>> e4d4eeebe1b68cbd647241326e2f060b4f77a073
             </CarouselItem>
           ))}
         </CarouselContent>
