f1d8568f8a2366295fab6d5c854c40233747a1f0
lordpluha <Tesluakevlad@gmail.com>
2026-01-07 18:24:22 +0100

    fix(infra): fix perfomance ci/cd

--- apps/api/src/seeds/seed.ts
+++ apps/api/src/seeds/seed.ts
@@ -1,6 +1,6 @@
-import 'dotenv/config'
 import { PrismaPg } from '@prisma/adapter-pg'
 import { PrismaClient } from '@prisma/client'
+import 'dotenv/config'
 import { Pool } from 'pg'
 
 import { seedAlbums } from './albums.seed'
