b7484650746d48d256ec39a6b168caeb2cd7b16d
lavanios <lavunov.danil@gmail.com>
2026-06-24 21:28:24 +0300

    feat(web-artists): added test only for web-artist

--- apps/web-artists/playwright.config.ts
+++ apps/web-artists/playwright.config.ts
@@ -12,7 +12,9 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: './tests',
+  testDir: './src',
+  testMatch: '**/__snapshots__/*.spec.ts',
+  snapshotPathTemplate: 'src/{testFileDir}/../__screenshot__/{arg}-{projectName}-{platform}{ext}',
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
---
+++ apps/web-artists/src/views/ArtistView/ui/__snapshots__/landing.snapshotTest.spec.ts
@@ -0,0 +1,51 @@
+import { test, expect } from '@playwright/test'
+
+test('visual regression with video pause', async ({ page }) => {
+  await page.emulateMedia({ reducedMotion: 'reduce' })
+  
+  await page.goto('http://localhost:3002/', { waitUntil: 'domcontentloaded' })
+  
+  const videoLocator = page.locator('video').first()
+  await videoLocator.waitFor({ state: 'visible' })
+
+  await page.evaluate(() => {
+    const videos = document.querySelectorAll('video');
+    videos.forEach(video => {
+      video.pause();       
+      video.currentTime = 0;
+    });
+  });
+
+  // Wait for all video pauesd
+  await page.waitForFunction(() =>
+    Array.from(document.querySelectorAll('video')).every(v => (v as HTMLVideoElement).paused),
+    null,
+    { timeout: 5000 },
+  );
+
+  // delay paused
+  await page.waitForTimeout(250);
+
+  await page.evaluate(() =>
+    document.documentElement.setAttribute('data-test-disable-transitions', '1')
+  )
+  await page.addStyleTag({
+    content: `
+      [data-test-disable-transitions] *,
+      [data-test-disable-transitions] *::before,
+      [data-test-disable-transitions] *::after {
+        transition: none !important;
+        animation: none !important;
+      }
+    `
+  })
+
+  await expect(page).toHaveScreenshot('landing.png', {
+    fullPage: true,
+    animations: 'disabled',
+    threshold: 0.1,
+    maxDiffPixels: 1000,
+    mask: [page.locator('[aria-roledescription="slide"]')],
+    timeout: 60000,
+  });
+})
