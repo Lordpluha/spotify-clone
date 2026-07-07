adfd2fa48b804b3bd5bb28943c698e741c91c0e3
lavanios <lavunov.danil@gmail.com>
2026-07-07 15:50:22 +0300

    feat(web-artists): added suspense and spinner for reset page

--- apps/web-artists/src/app/reset-password/page.tsx
+++ apps/web-artists/src/app/reset-password/page.tsx
@@ -1,9 +1,13 @@
+import { Suspense } from 'react'
+import { Spinner } from '@spotify/ui-react'
 import { ResetPasswordForm } from '@features/reset-password/ui/ResetPasswordForm'
 
 export default function ResetPasswordPage() {
   return (
     <main className="min-h-screen flex items-center justify-center p-8">
-      <ResetPasswordForm />
+      <Suspense fallback={<Spinner />}>
+        <ResetPasswordForm />
+      </Suspense>
     </main>
   )
 }
